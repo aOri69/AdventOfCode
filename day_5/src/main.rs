e557c386eb176f7b3c88581e4ba9f1ee8e94023c
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-06-27 00:18:03 +0400

    splitted into stacks and commands

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,3 +1,40 @@
+use supply_stacks::CrateStack;
+
 fn main() {
-    println!("Hello, world!");
+    let input_lines = include_str!("../input_test.txt").lines();
+
+    let stacks_str = input_lines
+        // Hope this is just an iterator clone
+        .clone()
+        .take_while(|line| !line.is_empty())
+        .collect::<Vec<_>>();
+    dbg!(&stacks_str);
+
+    //Determine number of stacks and create a vec of stacks
+    let stacks: Vec<CrateStack> = Vec::with_capacity(
+        stacks_str
+            .last()
+            .expect("Expected to have the enumeration of stacks")
+            .len()
+            / 3,
+    );
+    let len = stacks_str.len() + 1;
+
+    for stacks_line in stacks_str.into_iter().rev().skip(1) {
+        dbg!(stacks_line);
+        let r: Vec<_> = stacks_line
+            .split_ascii_whitespace()
+            .enumerate()
+            .map(|(idx, crt)| match crt.len() {
+                3 => Some(crt),
+                _ => None,
+            })
+            .collect();
+        dbg!(r);
+    }
+
+    dbg!(stacks);
+
+    let commands = input_lines.skip(len).collect::<Vec<_>>();
+    dbg!(commands);
 }
