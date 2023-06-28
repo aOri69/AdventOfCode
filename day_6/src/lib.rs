dd2a7fb66cdb853dc09081b4f4f276c2fa9b5d21
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-06-28 22:23:09 +0400

    solution based on kmp + on ring buffers

--- day_6/src/lib.rs
+++ day_6/src/lib.rs
@@ -1,36 +1,141 @@
 use std::collections::HashSet;
 
-/// # Purpose
+/// # My Solution
 /// Subroutine that detects a start-of-packet marker in the datastream.
 /// In the protocol being used by the Elves,
 /// the start of a packet is indicated
 /// by a sequence of four characters that are all different.
-pub fn get_marker_index(s: &str) -> usize {
-    for (idx, _c) in s.char_indices() {
-        let r = s.get(idx..idx + 4).unwrap();
-        dbg!(&r);
-        dbg!(is_different(r));
+pub fn get_marker_index(s: &str, seq_size: usize) -> Option<usize> {
+    let mut idx: usize = 0;
+    while let Some(s) = s.get(idx..idx + seq_size) {
+        // dbg!(s);
+        if let Some(advance) = find_duplicate_index(s) {
+            idx += advance + 1;
+            continue;
+        }
+        return Some(idx + seq_size);
     }
-    0
+    None
 }
 
-fn is_different(s: &str) -> bool {
-    let mut set = HashSet::new();
-    s.chars().for_each(|c| {
-        set.insert(c);
-    });
+fn find_duplicate_index(s: &str) -> Option<usize> {
+    let mut unique_chars = HashSet::new();
+    s.char_indices()
+        .find_map(|(i, c)| match unique_chars.insert(c).then_some(i) {
+            Some(_) => None,
+            None => s.chars().position(|i| i == c),
+        })
+}
+
+/// Ring buffer based solution
+pub fn marker_start(input: &str) -> usize {
+    const PREV_SIZE: usize = 3;
+    let mut prev = [' '; PREV_SIZE];
+    prev.copy_from_slice(&input.chars().collect::<Vec<_>>()[..PREV_SIZE]);
+    for (ix, c) in input.chars().skip(PREV_SIZE).enumerate() {
+        if !prev.contains(&c) && is_uniq(&prev) {
+            return ix + PREV_SIZE + 1;
+        } else {
+            prev[ix % PREV_SIZE] = c;
+        }
+    }
+
+    unreachable!("Input contains no message marker")
+}
+
+/// Ring buffer based solution
+pub fn message_start(input: &str) -> usize {
+    const PREV_SIZE: usize = 13;
+    let mut prev = [' '; PREV_SIZE];
+    prev.copy_from_slice(&input.chars().collect::<Vec<_>>()[..PREV_SIZE]);
+    for (ix, c) in input.chars().skip(PREV_SIZE).enumerate() {
+        if !prev.contains(&c) && is_uniq(&prev) {
+            return ix + PREV_SIZE + 1;
+        } else {
+            prev[ix % PREV_SIZE] = c;
+        }
+    }
+
+    unreachable!("Input contains no message marker")
+}
+
+fn is_uniq(s: &[char]) -> bool {
+    let set = s.iter().collect::<HashSet<_>>();
     matches!(set.len(), len if len == s.len())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    // Markers tests with 4
     #[test]
-    fn day_6_examples() {
-        assert_eq!(get_marker_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
-        assert_eq!(get_marker_index("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
-        assert_eq!(get_marker_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
-        assert_eq!(get_marker_index("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    fn test_marker_index_1() {
+        assert_eq!(
+            get_marker_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4),
+            Some(7)
+        );
+    }
+    #[test]
+    fn test_marker_index_2() {
+        assert_eq!(get_marker_index("bvwbjplbgvbhsrlpgdmjqwftvncz", 4), Some(5));
+    }
+    #[test]
+    fn test_marker_index_3() {
+        assert_eq!(get_marker_index("nppdvjthqldpwncqszvftbrmjlhg", 4), Some(6));
+    }
+    #[test]
+    fn test_marker_index_4() {
+        assert_eq!(
+            get_marker_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4),
+            Some(10)
+        );
+    }
+    #[test]
+    fn test_marker_index_5() {
+        assert_eq!(
+            get_marker_index("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4),
+            Some(11)
+        );
+    }
+    // Without markers
+    #[test]
+    fn test_without_markers() {
+        assert_eq!(get_marker_index("aaaabbbbccccd", 4), None);
+    }
+    // Message tests with 14
+    #[test]
+    fn test_message_index_1() {
+        assert_eq!(
+            get_marker_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14),
+            Some(19)
+        );
+    }
+    #[test]
+    fn test_message_index_2() {
+        assert_eq!(
+            get_marker_index("bvwbjplbgvbhsrlpgdmjqwftvncz", 14),
+            Some(23)
+        );
+    }
+    #[test]
+    fn test_message_index_3() {
+        assert_eq!(
+            get_marker_index("nppdvjthqldpwncqszvftbrmjlhg", 14),
+            Some(23)
+        );
+    }
+    #[test]
+    fn test_message_index_4() {
+        assert_eq!(
+            get_marker_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14),
+            Some(29)
+        );
+    }
+    #[test]
+    fn test_message_index_5() {
+        assert_eq!(
+            get_marker_index("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14),
+            Some(26)
+        );
     }
 }
--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,7 +1,12 @@
-use tuning_trouble::get_marker_index;
+use tuning_trouble::{get_marker_index, marker_start, message_start};
 
 fn main() {
     if let Ok(input) = std::fs::read_to_string("input.txt") {
-        println!("Signal marker index: {}", get_marker_index(&input));
+        println!("-------------My solution-------------");
+        println!("Signal marker index: {:?}", get_marker_index(&input, 4));
+        println!("Message index: {:?}", get_marker_index(&input, 14));
+        println!("-------------Ring buffer-------------");
+        println!("Signal marker index: {:?}", marker_start(&input));
+        println!("Message index: {:?}", message_start(&input));
     }
 }
