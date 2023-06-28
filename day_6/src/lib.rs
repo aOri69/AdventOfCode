e45266e8002444cd78a41e4290cc0f1cbcc5e1bd
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-06-28 18:03:51 +0400

    is_different() on a HashSet

--- day_6/src/lib.rs
+++ day_6/src/lib.rs
@@ -1,5 +1,25 @@
-pub fn process_signals(_s: &str) -> usize {
-    todo!()
+use std::collections::HashSet;
+
+/// # Purpose
+/// Subroutine that detects a start-of-packet marker in the datastream.
+/// In the protocol being used by the Elves,
+/// the start of a packet is indicated
+/// by a sequence of four characters that are all different.
+pub fn get_marker_index(s: &str) -> usize {
+    for (idx, _c) in s.char_indices() {
+        let r = s.get(idx..idx + 4).unwrap();
+        dbg!(&r);
+        dbg!(is_different(r));
+    }
+    0
+}
+
+fn is_different(s: &str) -> bool {
+    let mut set = HashSet::new();
+    s.chars().for_each(|c| {
+        set.insert(c);
+    });
+    matches!(set.len(), len if len == s.len())
 }
 
 #[cfg(test)]
@@ -8,9 +28,9 @@ mod tests {
 
     #[test]
     fn day_6_examples() {
-        assert_eq!(process_signals("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
-        assert_eq!(process_signals("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
-        assert_eq!(process_signals("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
-        assert_eq!(process_signals("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+        assert_eq!(get_marker_index("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(get_marker_index("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(get_marker_index("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(get_marker_index("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
     }
 }
--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -1,5 +1,7 @@
+use tuning_trouble::get_marker_index;
+
 fn main() {
     if let Ok(input) = std::fs::read_to_string("input.txt") {
-        dbg!(input);
+        println!("Signal marker index: {}", get_marker_index(&input));
     }
 }
