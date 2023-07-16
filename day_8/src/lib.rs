ef1ebb4f17f8affa63b50fdd6723a837f412c14c
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-07-16 19:12:41 +0400

    Day 8: Treetop Tree House

---
+++ day_8/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn part_1(_input: &str) -> u32 {
+    todo!("part1");
+}
+
+pub fn part_2(_input: &str) -> u32 {
+    todo!("part1");
+}
+
+#[cfg(test)]
+mod tests {
+    pub use super::*;
+
+    const TEST_INPUT: &str = "30373
+25512
+65332
+33549
+35390";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(TEST_INPUT), 21);
+    }
+
+    #[test]
+    fn test_part_2() {
+        // assert_eq!(part_2(TEST_INPUT), );
+    }
+}
