14916348921c6b404129e6e531f33964adec4537
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-07-31 23:52:31 +0400

    additional trait implementations for Position

--- day_9/src/rope.rs
+++ day_9/src/rope.rs
@@ -8,6 +8,50 @@ pub struct Position {
     pub y: i32,
 }
 
+impl From<(i32, i32)> for Position {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            x: value.0,
+            y: value.1,
+        }
+    }
+}
+
+impl From<Position> for (i32, i32) {
+    fn from(value: Position) -> Self {
+        (value.x, value.y)
+    }
+}
+
+impl std::ops::Add for Position {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl std::ops::AddAssign for Position {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl std::ops::Sub for Position {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct Rope {
     head: Position,
