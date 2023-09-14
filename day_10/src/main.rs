28e916f878b7a28007937e07700136696accfef5
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-09-14 22:23:07 +0400

    reference remove

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -2,6 +2,6 @@ use cathode_ray_tube::{draw_crt, sum_of_signal_strengths};
 
 fn main() {
     let input = include_str!("../input.txt");
-    println!("Part 1: {}", sum_of_signal_strengths(&input));
-    println!("Part 2:\n{}", draw_crt(&input).join("\n"));
+    println!("Part 1: {}", sum_of_signal_strengths(input));
+    println!("Part 2:\n{}", draw_crt(input).join("\n"));
 }
