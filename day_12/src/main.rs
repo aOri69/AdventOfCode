f0d62783c85a206251bba8ef1f3e37f190761b8a
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-11-17 18:01:54 +0400

    parser for height grid

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,3 +1,57 @@
+use std::convert::Infallible;
+
+#[derive(Debug, Clone)]
+enum Node {
+    Start,
+    End,
+    Path(u8),
+}
+
+impl From<char> for Node {
+    fn from(value: char) -> Self {
+        match value {
+            'S' => Self::Start,
+            'E' => Self::End,
+            c => Self::Path(c as u8 - b'a'),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Grid(Vec<Vec<Node>>);
+
+impl Grid {}
+
+impl std::str::FromStr for Grid {
+    type Err = Infallible; //Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        dbg!("{}", b'a');
+        let grid = s
+            .lines()
+            .map(|s| s.chars().map(Node::from).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        Ok(Self(grid))
+    }
+}
+
 fn main() {
+    let _input = include_str!("../input.txt");
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn small_input() {
+        let input = "Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi";
+        input.lines().for_each(|l| println!("{l}"));
+        let grid = Grid::from_str(input);
+        dbg!(grid.unwrap());
+        todo!()
+    }
 }
