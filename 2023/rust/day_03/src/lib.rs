684b3d5ffed668d66177ad45d214d629f7eae17b
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-12-06 22:32:53 +0400

    part 1 test passed

--- 2023/rust/day_03/src/lib.rs
+++ 2023/rust/day_03/src/lib.rs
@@ -13,38 +13,69 @@ pub fn part1(input: &str) -> u32 {
     '_rows: for (row, line) in input.lines().enumerate() {
         let mut cur_col = 0_usize;
         let mut number = String::new();
-        '_cols: for (col, c) in line.char_indices().filter_map(|(j, c)| match c {
-            c if c.is_ascii_digit() => Some((j, c)),
-            _ => None,
-        }) {
-            println!("(({},{})-{}) ", row, col, c);
+        let mut is_part_number = false;
 
+        let mut cols_it = get_ascii_digits_iter(line);
+
+        while let Some((col, c)) = cols_it.next() {
+            // '_cols: for (col, c) in cols_it {
+            print!("(({},{})-{}) ", row, col, c);
+
+            if !is_part_number {
+                is_part_number = get_neighbours(row, col).iter().any(|(row_n, col_n)| {
+                    // let coord = row_n * line_length + col_n;
+                    let c = input
+                        .lines()
+                        .nth(*row_n)
+                        .unwrap_or("")
+                        .chars()
+                        .nth(*col_n)
+                        .unwrap_or('.');
+                    println!("{row_n}-{col_n}-{c}");
+                    c != '.' && !c.is_ascii_digit()
+                });
+            }
+            let l = cols_it.peek();
             if col - cur_col == 1 || cur_col == 0 {
                 number.push(c);
                 cur_col = col;
             } else {
-                let is_part_number = get_neighbours(row, col)
-                    .iter()
-                    .inspect(|n| println!("({},{})", n.0, n.1))
-                    .any(|(row_n, col_n)| {
-                        // Actual coord in the stream of chars
-                        let coord = row_n * line_length + col_n;
-                        // PLACEHOLDER
-                        // TO-DO
-                        true
-                    });
                 if is_part_number {
                     part_numbers.push(number.parse().unwrap());
+                    is_part_number = false;
                 }
-                dbg!(&number);
+                number.clear();
+                cur_col = col;
+                number.push(c);
+
+                continue;
+            }
+
+            if cols_it.peek().is_none() {
+                if is_part_number {
+                    part_numbers.push(number.parse().unwrap());
+                    is_part_number = false;
+                }
                 number.clear();
                 cur_col = col;
                 number.push(c);
             }
         }
+        println!();
     }
 
     part_numbers.iter().sum()
+}
+
+fn get_ascii_digits_iter(
+    line: &str,
+) -> std::iter::Peekable<impl Iterator<Item = (usize, char)> + '_> {
+    line.char_indices()
+        .filter_map(|(j, c)| match c {
+            c if c.is_ascii_digit() => Some((j, c)),
+            _ => None,
+        })
+        .peekable()
 }
 
 fn get_neighbours(row: usize, col: usize) -> Vec<(usize, usize)> {
