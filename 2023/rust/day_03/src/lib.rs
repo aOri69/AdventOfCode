b3b4823f27a161c30cd49af1a173ed6ce7839faa
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-12-07 00:25:20 +0400

    Part1 complete

--- 2023/rust/day_03/src/lib.rs
+++ 2023/rust/day_03/src/lib.rs
@@ -13,58 +13,60 @@ pub fn part1(input: &str) -> u32 {
     '_rows: for (row, line) in input.lines().enumerate() {
         let mut cur_col = 0_usize;
         let mut number = String::new();
-        let mut is_part_number = false;
+        let mut is_part_flag = false;
 
         let mut cols_it = get_ascii_digits_iter(line);
 
         while let Some((col, c)) = cols_it.next() {
             // '_cols: for (col, c) in cols_it {
-            print!("(({},{})-{}) ", row, col, c);
+            // print!("(({},{})-{}) ", row, col, c);
 
-            if !is_part_number {
-                is_part_number = get_neighbours(row, col).iter().any(|(row_n, col_n)| {
-                    // let coord = row_n * line_length + col_n;
-                    let c = input
-                        .lines()
-                        .nth(*row_n)
-                        .unwrap_or("")
-                        .chars()
-                        .nth(*col_n)
-                        .unwrap_or('.');
-                    println!("{row_n}-{col_n}-{c}");
-                    c != '.' && !c.is_ascii_digit()
-                });
+            if !is_part_flag {
+                is_part_flag = is_part_number(input, row, col);
             }
-            let l = cols_it.peek();
+
             if col - cur_col == 1 || cur_col == 0 {
                 number.push(c);
                 cur_col = col;
-            } else {
-                if is_part_number {
-                    part_numbers.push(number.parse().unwrap());
-                    is_part_number = false;
-                }
-                number.clear();
-                cur_col = col;
-                number.push(c);
-
-                continue;
             }
-
-            if cols_it.peek().is_none() {
-                if is_part_number {
+            // Next digit not exitst in the line
+            // Or next digit is too far from current column(different number)
+            if cols_it.peek().is_none() || cols_it.peek().is_some_and(|(c, _)| c.abs_diff(col) > 1)
+            {
+                if is_part_flag {
                     part_numbers.push(number.parse().unwrap());
-                    is_part_number = false;
+                    is_part_flag = false;
                 }
                 number.clear();
-                cur_col = col;
-                number.push(c);
+                cur_col = 0;
             }
         }
-        println!();
     }
 
+    // part_numbers.iter().for_each(|n| {
+    //     println!("{n}");
+    // });
+    // let mut r = part_numbers
+    //     .iter()
+    //     .map(|n| n.to_string())
+    //     .collect::<Vec<String>>();
+    // r.sort();
+    // std::fs::write("non_working.txt", r.join("\n"));
     part_numbers.iter().sum()
+}
+
+fn is_part_number(input: &str, row: usize, col: usize) -> bool {
+    let is_part_number = get_neighbours(row, col).iter().any(|(row_n, col_n)| {
+        let c = input
+            .lines()
+            .nth(*row_n)
+            .unwrap_or("")
+            .chars()
+            .nth(*col_n)
+            .unwrap_or('.');
+        c != '.' && !c.is_ascii_digit()
+    });
+    is_part_number
 }
 
 fn get_ascii_digits_iter(
@@ -116,8 +118,21 @@ mod tests {
 .664.598..";
 
     #[test]
-    fn test_part1() {
+    fn test_part1_small() {
         assert_eq!(part1(INPUT), 4361);
+    }
+
+    #[test]
+    fn part_1_real() {
+        let input = include_str!("input.txt");
+        assert_eq!(part1(input), 525119);
+    }
+
+    #[test]
+    fn test_obvious() {
+        const CONT_INPUT: &str = "...&3..501.13..195......&.........
+........./....*.........11........";
+        assert_eq!(part1(CONT_INPUT), 710);
     }
 
     #[test]
