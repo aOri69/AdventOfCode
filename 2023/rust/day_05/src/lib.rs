34db6f143212f0ad81d1ae5b19fcc182b364b7e2
Aleksandr Sokolov <sashkaptits@gmail.com>
2023-12-10 12:45:24 +0400

    test case and empty functions

---
+++ 2023/rust/day_05/src/lib.rs
@@ -0,0 +1,57 @@
+pub fn part_1(_input: &str) -> u32 {
+    todo!("Part 1 implementation");
+}
+
+pub fn part_2(_input: &str) -> u32 {
+    todo!("Part 2 implementation");
+}
+
+#[cfg(test)]
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+
+    const INPUT: &str = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(INPUT), 35);
+    }
+
+    #[test]
+    fn test_part_2() {
+        todo!("part2 test function");
+    }
+}
--- 2023/rust/day_05/src/main.rs
+++ 2023/rust/day_05/src/main.rs
@@ -1,3 +1,7 @@
+use day_05::{part_1, part_2};
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input.txt");
+    println!("Part 1 answer: {}", part_1(input));
+    println!("Part 2 answer: {}", part_2(input));
 }
