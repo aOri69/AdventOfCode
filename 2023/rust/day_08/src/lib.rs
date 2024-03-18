f6f2df34f5a6b47e12994f034aeb37cb23a49393
Aleksandr Sokolov <sashkaptits@gmail.com>
2024-03-18 11:16:26 +0400

    simpliest input parser based on slices &'static str

--- 2023/rust/day_08/src/lib.rs
+++ 2023/rust/day_08/src/lib.rs
@@ -1,14 +1,138 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+type NodeName = &'static str;
+
+#[derive(PartialEq)]
+struct Node {
+    name: NodeName,
+    left: NodeName,
+    right: NodeName,
+}
+
+impl std::fmt::Debug for Node {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "{} = ({},{})",
+            self.name, self.left, self.right
+        ))
+    }
+}
+
+#[derive(PartialEq)]
+struct Network {
+    commands: &'static str,
+    nodes: Vec<Node>,
+}
+
+impl std::fmt::Debug for Network {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{}\n{:#?}", self.commands, self.nodes))
+    }
+}
+
+fn parse_input(input: &'static str) -> Network {
+    let mut input_it = input.lines();
+    let commands = input_it.next().expect("expected first row with commands");
+    let input_it = input_it.skip(1);
+
+    Network {
+        commands,
+        nodes: input_it
+            .map(|s| Node {
+                name: &s[..3],
+                left: &s[7..10],
+                right: &s[12..15],
+            })
+            .collect(),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
+
+    const TEST_INPUT1: &str = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+    const TEST_INPUT2: &str = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn parse_input1() {
+        let expected = Network {
+            commands: "RL",
+            nodes: vec![
+                Node {
+                    name: "AAA",
+                    left: "BBB",
+                    right: "CCC",
+                },
+                Node {
+                    name: "BBB",
+                    left: "DDD",
+                    right: "EEE",
+                },
+                Node {
+                    name: "CCC",
+                    left: "ZZZ",
+                    right: "GGG",
+                },
+                Node {
+                    name: "DDD",
+                    left: "DDD",
+                    right: "DDD",
+                },
+                Node {
+                    name: "EEE",
+                    left: "EEE",
+                    right: "EEE",
+                },
+                Node {
+                    name: "GGG",
+                    left: "GGG",
+                    right: "GGG",
+                },
+                Node {
+                    name: "ZZZ",
+                    left: "ZZZ",
+                    right: "ZZZ",
+                },
+            ],
+        };
+        assert_eq!(parse_input(TEST_INPUT1), expected);
+    }
+
+    #[test]
+    fn parse_input2() {
+        let expected = Network {
+            commands: "LLR",
+            nodes: vec![
+                Node {
+                    name: "AAA",
+                    left: "BBB",
+                    right: "BBB",
+                },
+                Node {
+                    name: "BBB",
+                    left: "AAA",
+                    right: "ZZZ",
+                },
+                Node {
+                    name: "ZZZ",
+                    left: "ZZZ",
+                    right: "ZZZ",
+                },
+            ],
+        };
+        assert_eq!(parse_input(TEST_INPUT2), expected);
     }
 }
