1610b7f2bd356a1d72ad8189568485044c66f12e
Aleksandr Sokolov <sashkaptits@gmail.com>
2024-03-18 17:39:45 +0400

    part1 done using Iterator and -1

--- 2023/rust/day_08/src/lib.rs
+++ 2023/rust/day_08/src/lib.rs
@@ -1,10 +1,30 @@
+use std::collections::HashSet;
+
 type NodeName = &'static str;
 
-#[derive(PartialEq)]
+const START_NODE: &str = "AAA";
+const END_NODE: &str = "ZZZ";
+
 struct Node {
     name: NodeName,
     left: NodeName,
     right: NodeName,
+}
+
+impl std::cmp::PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+impl std::cmp::Eq for Node {}
+
+impl std::hash::Hash for Node {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+        // self.left.hash(state);
+        // self.right.hash(state);
+    }
 }
 
 impl std::fmt::Debug for Node {
@@ -15,11 +35,94 @@ impl std::fmt::Debug for Node {
         ))
     }
 }
+struct NetworkIter<'a> {
+    network: &'a Network,
+    command_it: std::iter::Cycle<std::str::Chars<'a>>,
+    current_node: Option<&'a Node>,
+}
+
+impl<'a> Iterator for NetworkIter<'a> {
+    type Item = &'a Node;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.current_node {
+            Some(node) => match node.name {
+                END_NODE => None,
+                _ => match self.command_it.next() {
+                    Some(c) => {
+                        let result = match c {
+                            'L' => self.network.nodes.get(&Node {
+                                name: node.left,
+                                left: "",
+                                right: "",
+                            }),
+                            'R' => self.network.nodes.get(&Node {
+                                name: node.right,
+                                left: "",
+                                right: "",
+                            }),
+                            _ => unreachable!(),
+                        };
+                        self.current_node = result;
+                        result
+                    }
+                    None => unreachable!(),
+                },
+            },
+            None => {
+                self.current_node = self.network.nodes.get(&Node {
+                    name: START_NODE,
+                    left: "",
+                    right: "",
+                });
+                self.current_node
+            }
+        }
+    }
+
+    //     if let Some(current_node) = self.current_node {
+    //         if current_node.name == END_NODE {
+    //             return None;
+    //         }
+
+    //         let result = match self.command_it.next() {
+    //             Some(c) => match c {
+    //                 'L' => self.network.nodes.get(&Node {
+    //                     name: current_node.left,
+    //                     left: "",
+    //                     right: "",
+    //                 }),
+    //                 'R' => self.network.nodes.get(&Node {
+    //                     name: current_node.right,
+    //                     left: "",
+    //                     right: "",
+    //                 }),
+    //                 _ => unreachable!(),
+    //             },
+    //             None => return None,
+    //         };
+    //         self.current_node = result;
+    //         return result;
+    //     }
+
+    //     self.current_node
+    // }
+}
 
 #[derive(PartialEq)]
 struct Network {
     commands: &'static str,
-    nodes: Vec<Node>,
+    nodes: HashSet<Node>,
+}
+
+impl Network {
+    fn iter(&self) -> NetworkIter<'_> {
+        NetworkIter {
+            network: self,
+            command_it: self.commands.chars().cycle(),
+            current_node: None,
+        }
+    }
 }
 
 impl std::fmt::Debug for Network {
@@ -31,22 +134,28 @@ impl std::fmt::Debug for Network {
 fn parse_network(input: &'static str) -> Network {
     let mut input_it = input.lines();
 
-    Network {
-        commands: input_it.next().expect("expected first row with commands"),
-        nodes: input_it
-            .skip(1)
-            .map(|s| Node {
-                name: &s[..3],
-                left: &s[7..10],
-                right: &s[12..15],
-            })
-            .collect(),
-    }
+    let commands = input_it.next().expect("expected first row with commands");
+    let mut nodes = HashSet::new();
+    input_it
+        .skip(1)
+        .map(|s| Node {
+            name: &s[..3],
+            left: &s[7..10],
+            right: &s[12..15],
+        })
+        .for_each(|n| {
+            if !nodes.insert(n) {
+                panic!("Expected valid input");
+            }
+        });
+
+    Network { commands, nodes }
 }
 
 pub fn part1(input: &'static str) -> usize {
-    let _network = parse_network(input);
-    todo!();
+    let network = parse_network(input);
+    dbg!(&network);
+    network.iter().count() - 1
 }
 
 #[cfg(test)]
@@ -72,71 +181,103 @@ ZZZ = (ZZZ, ZZZ)";
 
     #[test]
     fn parse_input1() {
+        let mut nodes = HashSet::new();
+
+        vec![
+            Node {
+                name: "AAA",
+                left: "BBB",
+                right: "CCC",
+            },
+            Node {
+                name: "BBB",
+                left: "DDD",
+                right: "EEE",
+            },
+            Node {
+                name: "CCC",
+                left: "ZZZ",
+                right: "GGG",
+            },
+            Node {
+                name: "DDD",
+                left: "DDD",
+                right: "DDD",
+            },
+            Node {
+                name: "EEE",
+                left: "EEE",
+                right: "EEE",
+            },
+            Node {
+                name: "GGG",
+                left: "GGG",
+                right: "GGG",
+            },
+            Node {
+                name: "ZZZ",
+                left: "ZZZ",
+                right: "ZZZ",
+            },
+        ]
+        .into_iter()
+        .for_each(|n| {
+            nodes.insert(n);
+        });
+
         let expected = Network {
             commands: "RL",
-            nodes: vec![
-                Node {
-                    name: "AAA",
-                    left: "BBB",
-                    right: "CCC",
-                },
-                Node {
-                    name: "BBB",
-                    left: "DDD",
-                    right: "EEE",
-                },
-                Node {
-                    name: "CCC",
-                    left: "ZZZ",
-                    right: "GGG",
-                },
-                Node {
-                    name: "DDD",
-                    left: "DDD",
-                    right: "DDD",
-                },
-                Node {
-                    name: "EEE",
-                    left: "EEE",
-                    right: "EEE",
-                },
-                Node {
-                    name: "GGG",
-                    left: "GGG",
-                    right: "GGG",
-                },
-                Node {
-                    name: "ZZZ",
-                    left: "ZZZ",
-                    right: "ZZZ",
-                },
-            ],
+            nodes,
         };
         assert_eq!(parse_network(TEST_INPUT1), expected);
     }
 
     #[test]
     fn parse_input2() {
+        let mut nodes = HashSet::new();
+
+        vec![
+            Node {
+                name: "AAA",
+                left: "BBB",
+                right: "BBB",
+            },
+            Node {
+                name: "BBB",
+                left: "AAA",
+                right: "ZZZ",
+            },
+            Node {
+                name: "ZZZ",
+                left: "ZZZ",
+                right: "ZZZ",
+            },
+        ]
+        .into_iter()
+        .for_each(|n| {
+            nodes.insert(n);
+        });
+
         let expected = Network {
             commands: "LLR",
-            nodes: vec![
-                Node {
-                    name: "AAA",
-                    left: "BBB",
-                    right: "BBB",
-                },
-                Node {
-                    name: "BBB",
-                    left: "AAA",
-                    right: "ZZZ",
-                },
-                Node {
-                    name: "ZZZ",
-                    left: "ZZZ",
-                    right: "ZZZ",
-                },
-            ],
+            nodes,
         };
         assert_eq!(parse_network(TEST_INPUT2), expected);
+    }
+
+    #[test]
+    fn part1_input1() {
+        for element in parse_network(TEST_INPUT1).iter() {
+            dbg!(element);
+        }
+        assert_eq!(parse_network(TEST_INPUT1).iter().count() - 1, 2)
+    }
+
+    #[test]
+    fn part1_input2() {
+        for element in parse_network(TEST_INPUT2).iter() {
+            dbg!(element);
+        }
+        assert_eq!(parse_network(TEST_INPUT2).iter().count() - 1, 6)
     }
 }
