77199b6a38f397a6d24e0da2fb993f36353a3b62
Aleksandr Sokolov <sashkaptits@gmail.com>
2024-03-26 09:09:06 +0400

    empty functions and binary for part1

--- 2023/rust/day_10/src/lib.rs
+++ 2023/rust/day_10/src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! --- Day 10: Pipe Maze ---
+//!
+
+pub fn part1(_input: &str) -> usize {
+    todo!()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const TEST_SIMPLE: &str = ".....
+.S-7.
+.|.|.
+.L-J.
+.....";
+
+    const TEST_COMPLEX: &str = "..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...";
+
+    /// Distance map for simple case
+    /// .....
+    /// .012.
+    /// .1.3.
+    /// .234.
+    /// .....
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
+    fn part1_simple_input() {
+        let result = part1(TEST_SIMPLE);
         assert_eq!(result, 4);
+    }
+
+    /// Distance map for comlex case
+    /// ..45.
+    /// .236.
+    /// 01.78
+    /// 14567
+    /// 23...
+    #[test]
+    fn part1_complex_input() {
+        let result = part1(TEST_COMPLEX);
+        assert_eq!(result, 8);
     }
 }
