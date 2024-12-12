b46aa4800b91883ec1dec428fd313260611aeea0
Aleksandr Sokolov <Aleksandr.Sokolov@gazprom-mt.com>
2024-12-12 10:53:38 +0000

    day 2 part 2 done straightforward way

--- 2024/rust/day_02/src/lib.rs
+++ 2024/rust/day_02/src/lib.rs
@@ -17,16 +17,16 @@ pub fn part1(input: &str) -> u32 {
     let mut count = 0;
 
     for report in reports {
-        if is_report_valid(report.clone()) {
+        if is_report_valid(&report) {
             count += 1;
-            dbg!(report);
+            // dbg!(report);
         }
     }
 
     count
 }
 
-fn is_report_valid(report: Vec<u32>) -> bool {
+fn is_report_valid(report: &[u32]) -> bool {
     use Direction::*;
 
     let mut direction = None;
@@ -57,7 +57,39 @@ fn is_report_valid(report: Vec<u32>) -> bool {
 }
 
 pub fn part2(input: &str) -> u32 {
-    0
+    let reports = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|n| n.parse())
+                .collect::<Result<Vec<u32>, _>>()
+                .unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    let mut count = 0;
+
+    for report in reports {
+        // println!("rep - {report:?}");
+        if is_report_valid(&report) {
+            count += 1;
+        } else {
+            'inner_removal: for idx_to_remove in 0..report.len() {
+                let slice_before_split = &report[..idx_to_remove];
+                let slice_after_split = &report[idx_to_remove + 1..];
+                let new_vec = [slice_before_split, slice_after_split].concat();
+                // println!("bef - {slice_before_split:?}");
+                // println!("aft - {slice_after_split:?}");
+                // println!("new - {new_vec:?}");
+                if is_report_valid(&new_vec) {
+                    count += 1;
+                    break 'inner_removal;
+                }
+            }
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -79,5 +111,16 @@ mod tests {
     fn it_works2() {
         const INPUT: &str = "10 7 4 2 1";
         assert_eq!(part1(INPUT), 1)
+    }
+
+    #[test]
+    fn it_works3() {
+        const INPUT: &str = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+        assert_eq!(part2(INPUT), 4)
     }
 }
