d5fad1f7618b5f55fa917024ef93ca544e88cdf9
Aleksandr Sokolov <sashkaptits@gmail.com>
2024-12-12 12:11:02 +0000

    text fix

--- 2024/rust/day_02/src/main.rs
+++ 2024/rust/day_02/src/main.rs
@@ -3,5 +3,5 @@ use day_02::{part1, part2};
 fn main() {
     let input = include_str!("../input.txt");
     println!("Part 1: - {}", part1(input));
-    println!("Part 1: - {}", part2(input));
+    println!("Part 2: - {}", part2(input));
 }
