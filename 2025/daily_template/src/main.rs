718123974223af3cf790d80aa1fee003929bf4ca
Aleksandr Sokolov <sashkaptits@gmail.com>
2025-12-01 16:36:37 +0000

    conditional compilation for windows/linux paths

--- 2025/daily_template/src/main.rs
+++ 2025/daily_template/src/main.rs
@@ -1,7 +1,11 @@
 use {{project-name}}::{part1, part2};
 
 fn main() {
+    #[cfg(windows)]
+    let input = include_str!("..\\input.txt");
+    #[cfg(not(windows))]
     let input = include_str!("../input.txt");
+
     println!("Part 1: - {}", part1(input));
     println!("Part 2: - {}", part2(input));
 }
