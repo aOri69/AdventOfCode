8e001d559c050d10faf778a1adc5d819ab3392d3
Aleksandr Sokolov <sashkaptits@gmail.com>
2025-12-01 13:01:15 +0000

    day 01 part 1 complete

---
+++ 2025/day_01/src/lib.rs
@@ -0,0 +1,111 @@
+const DIAL_START: u8 = 50;
+
+struct Dial {
+    value: u8,
+    points_to_zero_times: usize,
+}
+
+impl Dial {
+    pub fn new(start_pos: u8) -> Dial {
+        Self {
+            value: start_pos,
+            points_to_zero_times: 0,
+        }
+    }
+
+    fn right(&mut self) -> u8 {
+        self.value = (self.value + 1) % 100;
+        self.value
+    }
+
+    fn right_by(&mut self, steps: usize) -> u8 {
+        // advance
+        self.value = ((self.value as usize + steps) % 100) as u8;
+        // check 0
+        if self.value == 0 {
+            self.points_to_zero_times += 1;
+        }
+        // return
+        self.value
+    }
+
+    fn left(&mut self) -> u8 {
+        // advance
+        self.value = if self.value == 0 { 99 } else { self.value - 1 };
+        // check 0
+        if self.value == 0 {
+            self.points_to_zero_times += 1;
+        }
+        // return
+        self.value
+    }
+
+    fn left_by(&mut self, steps: usize) -> u8 {
+        for _ in 0..steps {
+            self.value = if self.value == 0 { 99 } else { self.value - 1 };
+        }
+        if self.value == 0 {
+            self.points_to_zero_times += 1;
+        }
+        self.value
+    }
+
+    fn points_to_zero_times(&self) -> usize {
+        self.points_to_zero_times
+    }
+}
+
+impl DoubleEndedIterator for Dial {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        Some(self.left())
+    }
+}
+
+impl Iterator for Dial {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.right())
+    }
+}
+
+pub fn part1(input: &str) -> usize {
+    let mut dial = Dial::new(DIAL_START);
+
+    for line in input.lines() {
+        let (direction, steps) = line.split_at(1);
+        let steps = steps.parse::<usize>().expect("Expected to parse integer");
+
+        let _current_dial_value = match direction {
+            "L" => dial.left_by(steps),
+            "R" => dial.right_by(steps),
+            _ => panic!("Expected to parse the input"),
+        };
+    }
+
+    dial.points_to_zero_times()
+}
+
+pub fn part2(_: &str) -> usize {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        const TEST: &str = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82";
+        assert_eq!(part1(TEST), 3);
+    }
+}
---
+++ 2025/day_01/src/main.rs
@@ -0,0 +1,7 @@
+use day_01::{part1, part2};
+
+fn main() {
+    let input = include_str!("..\\input.txt");
+    println!("Part 1: - {}", part1(input));
+    println!("Part 2: - {}", part2(input));
+}
