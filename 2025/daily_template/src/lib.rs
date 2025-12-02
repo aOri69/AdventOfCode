60ec58bfb28496a87525b50197b8ce8f2779ef68
Aleksandr Sokolov <sashkaptits@gmail.com>
2025-12-02 09:08:24 +0000

    adjusted lib with unit tests

--- 2025/daily_template/src/lib.rs
+++ 2025/daily_template/src/lib.rs
@@ -1,7 +1,24 @@
 pub fn part1(input: &str) {
-    todo!();
+    todo!("Part 1 implementation");
 }
 
 pub fn part2(input: &str) {
-    todo!();
+    todo!("Part 2 implementation");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST: &str = "";
+
+    #[test]
+    fn test_part1() {
+        todo!("Part 1 UT");
+    }
+
+    #[test]
+    fn test_part2() {
+        todo!("Part 2 UT");
+    }
 }
