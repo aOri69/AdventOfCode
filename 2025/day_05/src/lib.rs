0e9c8440f6aa99f43c2bf39359b2725d56784827
Aleksandr Sokolov <sashkaptits@gmail.com>
2025-12-11 11:31:20 +0000

    day_05 part 2 complete

--- 2025/day_05/src/lib.rs
+++ 2025/day_05/src/lib.rs
@@ -16,6 +16,54 @@ impl Input {
             .cloned()
             .collect()
     }
+
+    fn get_amount_from_ranges_only(&self) -> usize {
+        // self.ranges
+        //     .iter()
+        //     .cloned()
+        //     .flatten()
+        //     .collect::<HashSet<Id>>()
+        //     .len()
+        self.ranges
+            .iter()
+            .fold(0usize, |acc, range| acc + *range.end() - *range.start() + 1)
+    }
+
+    fn normalize_ranges(&mut self) {
+        let cmp_start = |a: &RangeInclusive<Id>, b: &RangeInclusive<Id>| a.start().cmp(b.start());
+        self.ranges.sort_by(cmp_start);
+        // dbg!(&self.ranges);
+        let mut new_ranges: Vec<RangeInclusive<Id>> = vec![];
+        for range in &self.ranges {
+            let intersection = new_ranges
+                .iter()
+                .enumerate()
+                .find(|(_, r)| r.contains(range.start()));
+            // dbg!(&intersection);
+            match &intersection {
+                Some((idx, intersection_range)) => {
+                    // dbg!(&intersection_range, &range);
+                    if range.start() >= intersection_range.start()
+                        && range.end() <= intersection_range.end()
+                    {
+                        continue;
+                    } else if range.start() >= intersection_range.start() {
+                        let start_i = *intersection_range.start();
+                        // let end_i = *intersection_range.end();
+                        new_ranges.remove(*idx);
+                        new_ranges.push(start_i..=*range.start() - 1);
+                        new_ranges.push(range.clone());
+                    } else if range.start() < intersection_range.start() {
+                        unreachable!("should not happen")
+                    }
+                    // dbg!(&new_ranges);
+                }
+                None => new_ranges.push(range.clone()),
+            }
+        }
+        // dbg!(&new_ranges);
+        self.ranges = new_ranges;
+    }
 }
 
 pub fn part1(input: &str) -> usize {
@@ -23,8 +71,10 @@ pub fn part1(input: &str) -> usize {
     input.get_fresh_ids().len()
 }
 
-pub fn part2(_input: &str) -> usize {
-    todo!("Part 2 implementation");
+pub fn part2(input: &str) -> usize {
+    let mut input = parse_input(input);
+    input.normalize_ranges();
+    input.get_amount_from_ranges_only()
 }
 
 fn parse_input(input: &str) -> Input {
@@ -67,6 +117,14 @@ mod tests {
 32
 ";
 
+    //     3 4 5 6
+    //   2 3 4
+    //         5 6 7
+    const INTERSECTIONS: &str = "3-6
+5-7
+2-4
+";
+
     #[test]
     fn test_part1() {
         assert_eq!(part1(TEST), 3);
@@ -74,12 +132,14 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        todo!("Part 2 UT");
+        assert_eq!(part2(TEST), 14);
     }
 
-    // #[rstest]
-    // #[case("test", "test")]
-    // fn test_part1_case(#[case] input: &str, #[case] expected: &str) {
-    //     todo!()
-    // }
+    #[test]
+    fn test_part2_intersections() {
+        let mut input = parse_input(INTERSECTIONS);
+        input.normalize_ranges();
+        let result = input.get_amount_from_ranges_only();
+        assert_eq!(result, 6)
+    }
 }
