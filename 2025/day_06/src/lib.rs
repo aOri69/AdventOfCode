63185a13e99e7a4945acc6ec822954b091e3ee01
Aleksandr Sokolov <sashkaptits@gmail.com>
2025-12-12 09:31:48 +0000

    day_06 template

---
+++ 2025/day_06/src/lib.rs
@@ -0,0 +1,31 @@
+pub fn part1(input: &str) {
+    todo!("Part 1 implementation");
+}
+
+pub fn part2(input: &str) {
+    todo!("Part 2 implementation");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    const TEST: &str = "";
+
+    #[test]
+    fn test_part1() {
+        todo!("Part 1 UT");
+    }
+
+    #[test]
+    fn test_part2() {
+        todo!("Part 2 UT");
+    }
+
+    #[rstest]
+    #[case("test", "test")]
+    fn test_part1_case(#[case] input: &str, #[case] expected: &str) {
+        todo!()
+    }
+}
