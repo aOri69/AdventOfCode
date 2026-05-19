14bdb56ef4db76e6bcd490bffedd38b64f8c9ae3
Aleksandr Sokolov <sashkaptits@gmail.com>
2026-05-19 16:36:46 +0100

    day_07 part 1 complete not tested

--- 2025/day_07/src/lib.rs
+++ 2025/day_07/src/lib.rs
@@ -1,5 +1,57 @@
-pub fn part1(_input: &str) -> usize {
-    todo!("Part 1 implementation");
+use std::collections::HashSet;
+
+enum Manifold {
+    Start,
+    Splitter,
+    Space,
+    Beam,
+}
+
+impl From<char> for Manifold {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Self::Space,
+            '^' => Self::Splitter,
+            'S' => Self::Start,
+            '|' => Self::Beam,
+            _ => unreachable!("Should not be on the map"),
+        }
+    }
+}
+
+pub fn part1(input: &str) -> usize {
+    let mut splits_count = 0;
+    let mut beams = HashSet::new();
+    for (line_idx, line) in input.lines().enumerate() {
+        let mut debug_line = Vec::from_iter(line.chars());
+        if line_idx == 0 {
+            let starting_point_idx = line.find('S').expect("Expected to find starting point");
+            beams.insert(starting_point_idx);
+            continue;
+        }
+        for beam in beams.clone() {
+            match line.chars().nth(beam) {
+                Some('.') => {
+                    beams.insert(beam);
+                    debug_line[beam] = '|';
+                }
+                Some('^') => {
+                    beams.remove(&beam);
+                    splits_count += 1;
+                    if beam != 0 && beams.insert(beam - 1) {
+                        debug_line[beam - 1] = '|';
+                    }
+                    if beam != line.len() - 1 && beams.insert(beam + 1) {
+                        debug_line[beam + 1] = '|';
+                    }
+                }
+                Some(_) => unreachable!(),
+                None => unreachable!(),
+            }
+        }
+        println!("{}", String::from_iter(debug_line.iter()));
+    }
+    splits_count
 }
 
 pub fn part2(_input: &str) -> usize {
@@ -11,21 +63,21 @@ mod tests {
     use super::*;
 
     const TEST: &str = ".......S.......
-    ...............
-    .......^.......
-    ...............
-    ......^.^......
-    ...............
-    .....^.^.^.....
-    ...............
-    ....^.^...^....
-    ...............
-    ...^.^...^.^...
-    ...............
-    ..^...^.....^..
-    ...............
-    .^.^.^.^.^...^.
-    ...............";
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
 
     #[test]
     fn test_part1() {
